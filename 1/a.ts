fefba57b64ca695d3d479b6efac44eeed7acc1e1
Daniel <d.lopez4@sapalomera.cat>
2023-12-02 18:06:55 +0100

    Part 1 of day 1 done

--- 1/a.ts
+++ 1/a.ts
@@ -1,5 +1,19 @@
 export const a = "";
-
-const input = await Deno.readTextFile("input.txt");
-
-console.log("input");
+const input = await Deno.readTextFile("1/input.txt");
+const lines = input.split("\r\n")
+let totalSum = 0;
+console.log("Finding calibration values and adding them...");
+for (let i = 0; i < 10; i++) {
+    const line = lines[i].split("").filter((value) => {
+        const num = Number.parseInt(value)
+        if (!Number.isNaN(num)){
+            return value
+        }
+    });
+    console.log(lines[i]);
+    const calibrationValue = line[0] + line[line.length - 1];
+    console.log(calibrationValue);
+    totalSum += Number.parseInt(calibrationValue)
+}
+console.log(totalSum);
+    
