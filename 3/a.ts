02d2ff4751e9aa72a6ce110571ae0f02743f119a
Daniel <d.lopez4@sapalomera.cat>
2023-12-03 18:05:58 +0100

    Part 1 of day 3 done, that was tougher than I expected

---
+++ 3/a.ts
@@ -0,0 +1,80 @@
+export const a = "";
+
+const notOutOfBounds = (y: number, x: number): boolean => {
+    return (y < lines.length && y >= 0 ) && (x < lines[y].length && x >= 0);
+}
+
+const checkAdjacent = (y: number, x: number): number => {
+    const firstY = y == 0 ? y : y - 1;
+    const firstX = x == 0 ? x : x - 1;
+    //console.log("%s %s", firstY, firstX);
+    //console.log("%s %s", y, x);
+    let aNSum = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3 && firstY !== y && firstX !== x; j++) {
+            const currentY = firstY+i;
+            const currentX = firstX+j;
+            if(notOutOfBounds(currentY, currentX)){
+                const sChar = lines[currentY][currentX]
+                if(sChar.match(validNumber)){
+                    aNSum += findWholeNumber(sChar, currentY, currentX);
+                }
+            }
+        }
+    }
+    return aNSum;
+}
+
+const findWholeNumber = (initialNumber: string, y: number, x:number): number => {
+    let finalNumber = initialNumber;
+    let beforeX = x == 0 ? x : x - 1;
+    let afterX = x === lines[y].length - 1 ? x : x + 1;
+
+    /* 
+        Using checkAdjacent() inside these whiles prodcued an edge case when a num was right 
+        at the end of the array, solved it by adding o reducing by 1 the x value passed since 
+        y is never increased here and x does
+    */
+
+    while(lines[y][beforeX].match(validNumber)){
+        //console.log("Before Num found %s %s | %s", y, beforeX, lines[y][beforeX]);
+        finalNumber = lines[y][beforeX] + finalNumber;
+        lines[y][beforeX] = ".";
+        if(notOutOfBounds(y, beforeX - 1)){
+            beforeX--
+        }else{
+            break;
+        }
+    }
+    
+    while(lines[y][afterX].match(validNumber)){
+        //console.log("After Num found %s %s | %s", y, afterX, lines[y][afterX]);
+        finalNumber += lines[y][afterX];
+        lines[y][afterX] = ".";
+        if(notOutOfBounds(y, afterX + 1)){
+            afterX++;
+        }else{
+            break;
+        }
+    }
+    //console.log(finalNumber);
+    return Number.parseInt(finalNumber);
+}
+
+const input = await Deno.readTextFile("input.txt");
+const lines = input.split("\r\n").map(line => line.split(""));
+const validSymbol = new RegExp("^[^\\.\\w]", "s");
+const validNumber = new RegExp("\\d");
+let partNSum = 0;
+console.log("Finding all of the part numbers close to a symbol inside the engine schematic and summing them");
+
+for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    for (let j = 0; j < line.length; j++) {
+        const character = line[j];
+        if (character.match(validSymbol)) {
+            partNSum += checkAdjacent(i, j)
+        }
+    }
+}
+console.log(partNSum);
